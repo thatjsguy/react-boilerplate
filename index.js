51bf3870225b78347d469492a54affdc54b45ded
Stephen Marshall <smarshall1980@gmail.com>
2017-03-01 17:41:52 -0600

    add public dir and make static

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 const app = express();
 
+import path from 'path';
+
 const getPort = () => {
   switch(process.env.NODE_ENV) {
     case 'production':
@@ -12,12 +14,13 @@ const getPort = () => {
   }
 };
 const port = getPort();
-
-app.get('/', (req, res) => {
-  res.send('Hi!');
+app.use(express.static(path.join(__dirname, 'public'), {maxAge: '14d'}));
+app.use('/api', require('./api/index').default);
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
 });
 
-app.use('/api', require('./api/index').default);
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
