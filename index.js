c6d0eced4379be326808ca4016882a79a4a6bbe3
Stephen Marshall <smarshall1980@gmail.com>
2017-03-01 16:56:49 -0600

    basic Express running

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+const app = express();
+
+const getPort = () => {
+  switch(process.env.NODE_ENV) {
+    case 'production':
+      return 80;
+    break;
+    default:
+      return 3000;
+    break;
+  }
+};
+const port = getPort();
+
+app.get('/', (req, res) => {
+  res.send('Hi!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
