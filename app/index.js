bfe65cd31f7423652f7197a454bd743574894cd3
Stephen Marshall <smarshall1980@gmail.com>
2017-03-01 19:14:05 -0600

    add Webpack + React

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import { React, PropTypes } from 'react';
+import cx from 'classnames';
+
+export default class App extends React.Component {
+  render() {
+    <div>Hello</div>
+  }
+}
---
+++ webpack.config.babel.js
@@ -0,0 +1,18 @@
+import path from 'path';
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
